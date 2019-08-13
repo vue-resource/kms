6052cde77424d733c43a5607765d6f50d7db0408
杜印 <m18612326243@163.com>
2019-08-13 10:49:52 +0800

    style

--- src/views/contract/add.vue
+++ src/views/contract/add.vue
@@ -7,6 +7,7 @@ export default {
             categoryList:[],
             tmplList:[],
             nodelist:[],
+            titilList:['目标模版分类','选择目标库模版目标模版','选择节点库的节点模版'],
             projectId: this.$route.query.projectId,
             targetCategoryProps: {
                 children: "children",
@@ -113,22 +114,27 @@ export default {
 </script>
 <template>
   <div class="concactAdd">
-    <el-tree :data="categoryList" default-expand-all highlight-current :props="targetCategoryProps"
-      @node-click="clickCategory">
-    </el-tree>
-    <el-tree :data="tmplList" default-expand-all highlight-current :props="targetmodeProps"
-      @node-click="clickModel">
-    </el-tree>
-    <el-tree :data="nodelist" default-expand-all highlight-current show-checkbox :props="nodeProps"
-      ref="node-tree">
-    </el-tree>
-    <div class="submitBtn">
-      <div class="btn">
-       <el-button  class="goalTarget" @click="creatSubmit" type="primary">提交</el-button>         
-      </div>
-      <div class="cencel">
-        <el-button  class="goalTarget" @click="$router.back()" plain>取消</el-button>         
-      </div>
+      <ul class="Ul-title">
+        <li v-for="(item, idx) in titilList" :key="idx" >{{item}}</li>
+      </ul>
+       <div class="tree-box">
+          <el-tree :data="categoryList" default-expand-all highlight-current :props="targetCategoryProps"
+            @node-click="clickCategory">
+          </el-tree>
+          <el-tree :data="tmplList" default-expand-all highlight-current :props="targetmodeProps"
+            @node-click="clickModel">
+          </el-tree>
+          <el-tree :data="nodelist" default-expand-all highlight-current show-checkbox :props="nodeProps"
+            ref="node-tree">
+          </el-tree>
+          <div class="submitBtn">
+            <div class="btn">
+            <el-button  class="goalTarget" @click="creatSubmit" type="primary">提交</el-button>         
+            </div>
+            <div class="cencel">
+              <el-button  class="goalTarget" @click="$router.back()" plain>取消</el-button>         
+            </div>
+          </div>
     </div>
   </div>
 </template>
@@ -137,20 +143,93 @@ export default {
   padding:20px;
 }
 .concactAdd{
-  display: flex;
+  // display: flex;
   margin-top:50px;
+  .Ul-title{
+    display: flex;
+     li{
+       width: 230px;
+       background: #9dc5f8;
+       text-align: center;
+       color: aliceblue;
+       padding: 4px;
+      position: relative;
+      &:first-child{
+         background: #409EFF;
+          &:first-child::after{
+            width: 0;
+            height:0;
+            content: '';
+            position: absolute;
+            right:-15px;
+            top:0px;
+           z-index: 200;
+            border-top:15px solid transparent;
+            border-bottom: 15px solid transparent;
+            border-left: 15px solid #409EFF;
+          }
+      }
+        &:nth-child(3){
+         background: #409EFF;
+          &:nth-child(3)::after{
+            width: 0;
+            height:0;
+            content: '';
+            position: absolute;
+            right:-15px;
+            top:0px;
+            z-index: 18;
+            border-top:15px solid transparent;
+            border-bottom: 15px solid transparent;
+            border-left: 15px solid #409EFF;
+          }
+      }
+       &:nth-child(2){
+         background: #9dc5f8;
+          &:nth-child(2)::after{
+            width: 0;
+            height:0;
+            content: '';
+            position: absolute;
+            right:-15px;
+            top:0px;
+            z-index: 210;
+            border-top:15px solid transparent;
+            border-bottom: 15px solid transparent;
+            border-left: 15px solid #9dc5f8;
+          }
+      }
+       &::after{
+        width: 0;
+        height:0;
+        content: '';
+        position: absolute;
+        right:-15px;
+        top:0px;
+        z-index:22px;
+        border-top:15px solid transparent;
+        border-bottom: 15px solid transparent;
+        border-left: 15px solid #9dc5f8;
+      }
+     }
+  }
+  .tree-box{
+    display: flex;
+    
+  }
   .el-tree {
     width:200px;
     border: 1px solid #ccc;
     overflow: auto;
     min-height: 200px;
     max-height: 800px;
-    margin-left: 20px;
+    margin-left: 30px;
     &:first {margin-left: 0;}
   }
   
   .submitBtn{
     margin-left:30px;
+    margin-top:20px;
     .btn{
       width:200px;
       display: block;
